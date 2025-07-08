85a8195a4d05b01af1b330ee01b1ed562c9e0306
Phuoc Nguyen <phuoc.nguyenquang.34@gmail.com>
2025-07-08 10:09:50 -0400

    add finetuning scripts for baselines

---
+++ training_scripts/slurm_ft-gemma_2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --gpus-per-node=a100:2
+#SBATCH --mem=128G
+#SBATCH --time=144:00:00
+#SBATCH --account=def-annielee
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=quangphuoc.nguyen@ontariotechu.net
+#SBATCH --output=outfile/finetuning-gemma_2-%j.out
+
+#############################################################
+# install the environment by loading in python and required packages
+module load StdEnv/2023 gcc/12.3 python/3.10.13 cuda/12.2 arrow/17.0.0 
+
+source /home/bumie304/scratch/QAlign/ENV/bin/activate
+#############################################################
+
+# stage 1: question alignment
+# finetuning model on question translation data
+bash training_scripts/finetune_modified.sh gemma-2-9b-it gsm8kafri_all
+
+# stage 2: response alignment
+# finetuning stage 1 model with MetaMathQA dataset
+bash training_scripts/finetune_modified.sh gemma-2-9b-it.gsm8kafri_all.finetune metamath_all
+
+# EVALUATION MGSM
+MODEL="gemma-2-9b-it.gsm8kafri_all.finetune.metamath_all.finetune"
+
+PROJECT_PATH="$(pwd)"
+SCRATCH_PATH=~/scratch/QAlign
+MODEL_PATH=$SCRATCH_PATH/model/$MODEL
+
+# For 13B model, you may need to set batch_size smaller, like 16, to avoid OOM issue.
+python $PROJECT_PATH/evaluate/scripts/afrimgsm_test.py \
+    --model_path $MODEL_PATH \
+    --streategy Parallel \
+    --batch_size 32 \
---
+++ training_scripts/slurm_ft-metamath_mgsm.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --gpus-per-node=a100:2
+#SBATCH --mem=128G
+#SBATCH --time=158:00:00
+#SBATCH --account=def-annielee
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=quangphuoc.nguyen@ontariotechu.net
+#SBATCH --output=outfile/finetuning-metamath_mgsm-%j.out
+
+#############################################################
+# install the environment by loading in python and required packages
+module load StdEnv/2023 gcc/12.3 python/3.10.13 cuda/12.2 arrow/17.0.0 
+
+source /home/bumie304/scratch/QAlign/ENV/bin/activate
+#############################################################
+
+# stage 1: question alignment
+# finetuning model on question translation data
+bash training_scripts/finetune_modified.sh MetaMath-Mistral-7B gsmtrans_gsm8kinstruct_question_all-en
+
+# stage 2: response alignment
+# finetuning stage 1 model with MetaMathQA dataset
+bash training_scripts/finetune_modified.sh MetaMath-Mistral-7B.gsmtrans_gsm8kinstruct_question_all-en.finetune metamath_all
+
+# EVALUATION MGSM
+MODEL="MetaMath-Mistral-7B.gsmtrans_gsm8kinstruct_question_all-en.finetune.metamath_all.finetune"
+
+PROJECT_PATH="$(pwd)"
+SCRATCH_PATH=~/scratch/QAlign
+MODEL_PATH=$SCRATCH_PATH/model/$MODEL
+
+# For 13B model, you may need to set batch_size smaller, like 16, to avoid OOM issue.
+python $PROJECT_PATH/evaluate/scripts/mgsm_test.py \
+    --model_path $MODEL_PATH \
+    --streategy Parallel \
+    --batch_size 16 \
+
+python $PROJECT_PATH/evaluate/scripts/msvamp_test.py \
+    --model_path $MODEL_PATH \
+    --streategy Parallel \
+    --batch_size 16 \
